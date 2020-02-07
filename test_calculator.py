3977c5caedabfd1739af354fc174851745b04e66
Anna <annaso3@kth.se>
2020-02-07 12:47:52 +0100

    Fix typo

--- test_calculator.py
+++ test_calculator.py
@@ -7,8 +7,8 @@ import calculator
 import pytest
 
 
-def test_addition(self):
+def test_addition():
     assert 4 == calculator.add(2, 2)
 
-def test_subtraction(self):
+def test_subtraction():
     assert 2 == calculator.subtract(4, 2)
